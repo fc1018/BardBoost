a20d9c2b4e40e4870dd93a0ecb105cd8958ae66f
Felix <fc1018@ic.ac.uk>
2023-05-22 14:11:06 +0100

    onload method test

--- boost.js
+++ boost.js
@@ -1,25 +1,26 @@
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function fillQuery() {
-  let url = new URL(document.location);
-  let question = url.searchParams.get('question');
-  
-  if(!question) {
-    return;
-  }
+	let url = new URL(document.location);
+	let question = url.searchParams.get('question');
 
-  await sleep(200); // Time to load page
+	if (!question) {
+		return;
+	}
 
-  var input = document.getElementsByTagName("textarea")[0];
-  input.value = question;
+	// await sleep(200); // Time to load page
 
-  submit = document.getElementsByClassName(
-    "mat-mdc-tooltip-trigger send-button mdc-icon-button mat-mdc-icon-button mat-primary mat-mdc-button-base gmat-mdc-button ng-star-inserted")[0];
+	var input = document.getElementsByTagName('textarea')[0];
+	input.value = question;
 
-  input.dispatchEvent(new Event('input', {bubbles:true}));
-  submit.click();
+	submit = document.getElementsByClassName(
+		'mat-mdc-tooltip-trigger send-button mdc-icon-button mat-mdc-icon-button mat-primary mat-mdc-button-base gmat-mdc-button ng-star-inserted'
+	)[0];
+
+	input.dispatchEvent(new Event('input', { bubbles: true }));
+	submit.click();
 }
 
-fillQuery();
+window.onload = fillQuery();
