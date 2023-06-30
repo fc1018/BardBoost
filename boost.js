0885640937fabd97781be6b12a9b80f3111b84d0
Felix <mail@felixchan.net>
2023-06-30 01:29:58 +0100

    Removed sleep function and added event listener

--- boost.js
+++ boost.js
@@ -1,28 +1,24 @@
-function sleep(ms) {
-	return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 async function fillQuery() {
+	//Fetch query from url
 	let url = new URL(document.location);
 	let question = url.searchParams.get('question');
-
-	if (!question) {
-		return;
+  
+	if(!question) {
+	  return;
 	}
-
-	document.onreadystatechange = () => {
-		if (document.readyState !== "complete") {
-			return
-		}
-		var input = document.getElementsByTagName('textarea')[0];
+	//Document load status check
+	document.addEventListener("readystatechange", (event) => {
+	  if (document.readyState === "interactive" || document.readyState === "complete") {
+		//Find text field and insert query
+		var input = document.getElementsByTagName("textarea")[0];
 		input.value = question;
-
+		//Find button
 		submit = document.getElementsByClassName(
-			'mat-mdc-tooltip-trigger send-button mdc-icon-button mat-mdc-icon-button mat-primary mat-mdc-button-base gmat-mdc-button ng-star-inserted'
-		)[0];
-		input.dispatchEvent(new Event('input', { bubbles: true }));
+		"mat-mdc-tooltip-trigger send-button mdc-icon-button mat-mdc-icon-button mat-primary mat-mdc-button-base gmat-mdc-button ng-star-inserted")[0];
+		//Trigger button
+		input.dispatchEvent(new Event('input', {bubbles:true}));
 		submit.click();
-	};
-}
-
-fillQuery();
+	  }
+	});
+  }
+  fillQuery();
