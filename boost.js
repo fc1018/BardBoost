64741708eb524b1106cf4835e3bc0f27583b3a2c
Felix <mail@felixchan.net>
2023-11-21 17:19:48 +0000

    added setTimeout to fix

--- boost.js
+++ boost.js
@@ -1,31 +1,35 @@
-function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
+async function queryEntry(question) {
+	console.log("running queryEntry", question);
+	//Find text field and insert query
+	let input = document.querySelector("rich-textarea > * > p");
+	console.log("Printing target element:");
+	console.log(input);
+	input.textContent = question;
+	//Find button and click
+	setTimeout(() => {
+		let submit = document.querySelector("input-area > div > div:nth-child(3) > button");
+		console.log("Printing button element:");
+		console.log(submit);
+		console.log("Clicking button...");
+		submit.click();
+		console.log("Button clicked.");
+	}, 50);
 }
 
 async function fillQuery() {
 	//Fetch query from url
-	let url = new URL(document.location);
-	let question = url.searchParams.get('question');
-
+	const params = new Proxy(new URLSearchParams(window.location.search), {
+		get: (searchParams, prop) => searchParams.get(prop),
+	});
+	let question = params.question;
+	console.log("Printing Query:");
+	console.log(question);
 	if (!question) {
 		return;
 	}
 	//Document load status check
-	document.addEventListener("readystatechange", (event) => {
-		if (document.readyState === "interactive" || document.readyState === "complete") {
-			//Find text field and insert query
-			var input = document.querySelector(
-				"body > chat-app > side-navigation > mat-sidenav-container > mat-sidenav-content > main > chat-window > div.chat-container.ng-tns-c1925132332-2.ng-star-inserted > div.bottom-container.ng-tns-c1925132332-2.ng-star-inserted > div.input-area-container.ng-tns-c1925132332-2 > input-area > div > div.text-input-field.ng-star-inserted > div > div > rich-textarea > div.ql-editor.textarea > p"
-			);
-			console.log(input);
-			input.textContent = question;
-			//Find button and click
-			document.querySelector(
-				"body > chat-app > side-navigation > mat-sidenav-container > mat-sidenav-content > main > chat-window > div.chat-container.ng-tns-c1925132332-2.ng-star-inserted > div.bottom-container.ng-tns-c1925132332-2.ng-star-inserted > div.input-area-container.ng-tns-c1925132332-2 > input-area > div > div.send-button-container.outer.ng-star-inserted > button > span.mat-mdc-button-touch-target"
-			).click();
-		}
-	});
+	window.onload = (event) => {
+		setTimeout(() => queryEntry(question), 100)
+	};
 }
-fillQuery();
-// For some reason running the function a second time achieves the intended target
 fillQuery();
