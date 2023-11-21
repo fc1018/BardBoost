2f6830c1c5ca06cc9024f8db7dbf0ae930753218
Felix <mail@felixchan.net>
2023-11-21 17:55:24 +0000

    removed logs

--- boost.js
+++ boost.js
@@ -1,18 +1,11 @@
 async function queryEntry(question) {
-	// console.log("running queryEntry", question);
 	//Find text field and insert query
 	let input = document.querySelector("rich-textarea > * > p");
-	// console.log("Printing target element:");
-	// console.log(input);
 	input.textContent = question;
 	//Find button and click
 	setTimeout(() => {
 		let submit = document.querySelector("input-area > div > div:nth-child(3) > button");
-		// console.log("Printing button element:");
-		// console.log(submit);
-		// console.log("Clicking button...");
 		submit.click();
-		// console.log("Button clicked.");
 	}, 50);
 }
 
@@ -22,8 +15,6 @@ async function fillQuery() {
 		get: (searchParams, prop) => searchParams.get(prop),
 	});
 	let question = params.question;
-	// console.log("Printing Query:");
-	// console.log(question);
 	if (!question) {
 		return;
 	}
