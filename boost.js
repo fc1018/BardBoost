85cf67298b404014d5308fd3dbdc147a37f4f3cc
Felix <mail@felixchan.net>
2023-11-21 17:27:18 +0000

    Commented out console logs

--- boost.js
+++ boost.js
@@ -1,18 +1,18 @@
 async function queryEntry(question) {
-	console.log("running queryEntry", question);
+	// console.log("running queryEntry", question);
 	//Find text field and insert query
 	let input = document.querySelector("rich-textarea > * > p");
-	console.log("Printing target element:");
-	console.log(input);
+	// console.log("Printing target element:");
+	// console.log(input);
 	input.textContent = question;
 	//Find button and click
 	setTimeout(() => {
 		let submit = document.querySelector("input-area > div > div:nth-child(3) > button");
-		console.log("Printing button element:");
-		console.log(submit);
-		console.log("Clicking button...");
+		// console.log("Printing button element:");
+		// console.log(submit);
+		// console.log("Clicking button...");
 		submit.click();
-		console.log("Button clicked.");
+		// console.log("Button clicked.");
 	}, 50);
 }
 
@@ -22,8 +22,8 @@ async function fillQuery() {
 		get: (searchParams, prop) => searchParams.get(prop),
 	});
 	let question = params.question;
-	console.log("Printing Query:");
-	console.log(question);
+	// console.log("Printing Query:");
+	// console.log(question);
 	if (!question) {
 		return;
 	}
